4599c54e72226ee5a7c621997fb65e413323c7d2
jscardona42 <35873185+jscardona42@users.noreply.github.com>
2020-06-25 09:00:07 -0500

    Carga inicial del proyecto
    Carga inicial para la primera sustentación.

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$("#btnModalSignIn").off("click").on("click", function () {
+    $("#modalSignIn").modal("show");
+})
+
+$("#btnModalSignUp").off("click").on("click", function () {
+    $("#modalSignUp").modal("show");
+})
