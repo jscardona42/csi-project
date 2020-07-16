5bd9f18b149e3231419d58fdbe7d9f21af12b162
jscardona42 <35873185+jscardona42@users.noreply.github.com>
2020-07-16 11:18:30 -0500

    Se agrega dashboard

--- js/index.js
+++ js/index.js
@@ -5,3 +5,17 @@ $("#btnModalSignIn").off("click").on("click", function () {
 $("#btnModalSignUp").off("click").on("click", function () {
     $("#modalSignUp").modal("show");
 })
+
+$("#btnSignIn").off("click").on("click", function () {
+    var usuario = $("#usuario").val();
+    var clave = $("#clave").val();
+    if (usuario == "admin" && clave == "1234") {
+        location.href = "dashboard/eventos.html";
+    }else if (usuario == "user" && clave == "1234") {
+        location.href = "dashboard/index2.html";
+    }
+    else{
+        alert("Contraseña incorrecta");
+    }
+})
+
